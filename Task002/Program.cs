88c2014a74e4178adfb1f82a3e161c1ac2fc90de
Ilya <ilya5946096@gmail.com>
2023-01-27 01:27:19 +0300

    Task002

--- Task002/Program.cs
+++ Task002/Program.cs
@@ -1,3 +1,21 @@
-﻿
+﻿//напишите программу которая будет проверять является ли первое число квадратом второго
+Console.WriteLine("Введите число 1: ");
+
+int number = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число 2: ");
+//Console.WriteLine($"Квадрат числа {number} = {square}");
+int number1 = Convert.ToInt32(Console.ReadLine());
 
 
+if (number1*number1==number)
+{
+    
+    Console.WriteLine("да");
+}
+
+else
+
+{
+   Console.WriteLine("печаль"); 
+}
